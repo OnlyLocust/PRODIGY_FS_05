f4f6f3634e11e61585209cdf7f6599fc8550da9a
Harsh Kr <harshchudasama4739s@gmail.com>
2025-02-28 01:47:11 +0530

    frontend setup test

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,23 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import {BrowserRouter} from 'react-router-dom'
-import { Provider } from 'react-redux'
-import store from '../store/store.js'
-import cors from 'cors'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "../store/store.js";
+import cors from "cors";
 
 cors({
-  origin:'https://posty-0rlh.onrender.com',
-  credentials:true
-})
+  origin: "https://posty-0rlh.onrender.com",
+  credentials: true,
+});
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   // <StrictMode>
-    <BrowserRouter  basename="/">
+  <BrowserRouter basename="/">
     <Provider store={store}>
-    <App />
+      <App />
     </Provider>
-    </BrowserRouter>
+  </BrowserRouter>
   // </StrictMode>,
-)
+);
