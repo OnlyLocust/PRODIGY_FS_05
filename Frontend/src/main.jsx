53d303ebb2779b06e79ee2442417281549b6374c
Harsh Kr <harshchudasama4739s@gmail.com>
2025-02-28 01:44:32 +0530

    frontend setup test

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -14,7 +14,7 @@ cors({
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter  basename="/home">
     <Provider store={store}>
     <App />
     </Provider>
